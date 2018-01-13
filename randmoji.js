286bd73fd0be566fa8cce08882f78a19e4e14ee1
Matthew Nau <matthewnau@users.noreply.github.com>
2018-01-13 16:15:31 -0500

    componetize functions
    - split tasks up into multiple components

--- randmoji.js
+++ randmoji.js
@@ -6,52 +6,68 @@ function randInt(max) {
 	return Math.floor(Math.random() * max);
 }
 
+// return the length of a json object
 function jLen(object) {
 	return Object.keys(object).length;
 }
 
-function randBitmoji(sex) {
-	let pd2 = [];
-	let colours = [];
-	let body = [];
-	let url = `https://render.bitstrips.com//render/${comics.imoji[randInt(comics.imoji.length)].comic_id}/316830037_16_s4-v1.png?`;
+// convert a json object to string
+function jStr(object) {
+	return JSON.stringify(object);
+}
 
-	//get all JSON elements
-	for (let i = 0; i < jLen(bitmoji[sex]); i++) {
-		let counter = Object.keys(bitmoji[sex])[i];
-		if (typeof bitmoji[sex][counter] === 'object') {
-			const attribute = JSON.stringify(bitmoji[sex][counter][randInt(bitmoji[sex][counter].length)]);
-			pd2.push(attribute.substr(1,attribute.length-2));
+function genColors(avatar, sex) {
+	let colors = [];
+	for (let i = 0; i < jLen(avatar[sex]['colors']); i++) {
+		let color = `"${avatar[sex]['colors'][i][Object.keys(avatar[sex]['colors'][i])[0]]}":${randInt(16777216)}`;
+		colors.push(color);
+	}
+	return colors;
+}
+
+function genAttributes(avatar, sex) {
+	let attributes = [];
+	for (let i = 0; i < jLen(avatar[sex]); i++) {
+		let counter = Object.keys(avatar[sex])[i];
+		if (typeof avatar[sex][counter] === 'object') {
+			const attribute = jStr(avatar[sex][counter][randInt(avatar[sex][counter].length)]);
+			attributes.push(attribute.substr(1,attribute.length-2));
 		}
 	}
+	return attributes.slice(0,attributes.length-(bitmoji[sex]['sex']+3));
+}
 
-	const build = JSON.stringify(bitmoji[sex]['build'][randInt(jLen(bitmoji[sex]['build']))]);
+function genBody(avatar, sex) {
+	let body = [];
+
+	const build = jStr(bitmoji[sex]['build'][randInt(jLen(bitmoji[sex]['build']))]);
 	body.push(build.substr(1,build.length-2));
-
-	for (let i = 0; i < jLen(bitmoji[sex]['colors']); i++) {
-		let color = '"'+bitmoji[sex]['colors'][i][Object.keys(bitmoji[sex]['colors'][i])[0]];
-		color += '":' + randInt(16777216);
-		colours.push(color);
-	}
-
-	//remove the JSON elements not related to pd2
-	if (sex === 'male') {
-		pd2 = pd2.slice(0,pd2.length-4);
-	}
-	else {
-		pd2 = pd2.slice(0,pd2.length-5);
-		const chestSize = JSON.stringify(bitmoji[sex]['chestSize'][randInt(jLen(bitmoji[sex]['chestSize']))]);
+	if (sex !== 'male') {
+		const chestSize = jStr(bitmoji[sex]['chestSize'][randInt(jLen(bitmoji[sex]['chestSize']))]);
 		body.push(chestSize.substr(1,chestSize.length-2));
 	}
+	return body;
+}
 
-	url += `pd2={${pd2}}&colours={${colours}}&body={${body}}&sex=${bitmoji[sex]['sex']}`;
-	url += `&outfit=${bitmoji[sex]['outfit'][randInt(bitmoji[sex]['outfit'].length)]}`;
-	url += `&proportion=${bitmoji[sex]['proportion'][randInt(bitmoji[sex]['proportion'].length)]}`;
-	url += '&cropped="body"&scale=3&style=4'
+function genUrl(avatar, sex) {
+	if (avatar === 'bitmoji') {
+		avatar = bitmoji;
+	}
+
+	let comic = comics.imoji[randInt(comics.imoji.length)].comic_id;
+	let attributes = genAttributes(avatar,sex);
+	let colors = genColors(avatar,sex);
+	let body = genBody(avatar,sex);
+	let outfit = avatar[sex]['outfit'][randInt(avatar[sex]['outfit'].length)];
+	let proportion = avatar[sex]['proportion'][randInt(avatar[sex]['proportion'].length)];
+	let scale = 3;
+	sex = avatar[sex]['sex'];
+
+	let url = `https://render.bitstrips.com//render/${comic}/316830037_16_s4-v1.png?pd2={${attributes}}&colours={${colors}}&body={${body}}&sex=${sex}&outfit=${outfit}&proportion=${proportion}&cropped="body"&scale=${scale}&style=4`;
 
 	return url;
 }
 
 if (args.length > 2 && args.length < 4) {
-	console.log(randBitmoji(args[2]));
+	console.log(genUrl('bitmoji',args[2]));
 }
